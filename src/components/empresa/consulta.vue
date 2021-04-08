13269608cc17f38e937b9fc7b40da347fde318ae
flavio <>
2021-04-08 00:26:17 -0300

    organização compoente empresa

---
+++ src/components/empresa/consulta.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>My component</div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: 'ConsultaEmpresa',
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState({
+      mostraConsulta: state => state.app.mostraConsulta,
+      currentMenu: state => state.app.currentMenu,
+      filtroTabela: state => state.app.filtroTabela
+    })
+  },
+  methods: {
+    ...mapActions({
+      setCepSelecionado: 'app/setCepSelecionado',
+      setMostraConsulta: 'app/setMostraConsulta'
+    }),
+  }
+}
+</script>
