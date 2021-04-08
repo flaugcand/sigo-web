2401bcea2cfe685c0a313535c2fe4f4ae8116b18
flavio <>
2021-04-08 01:11:08 -0300

    Container Principal

---
+++ src/components/menu/principal.vue
@@ -0,0 +1,31 @@
+<template>
+  <component :is="currentMenu" ref="menuComponent"/>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+import MenuEmpresa from 'src/components/menu/empresa'
+
+export default {
+  name: 'Principal',
+  components: {
+    MenuEmpresa
+  },
+
+  methods: {
+    ...mapActions({
+      setCurrentMenu: 'app/setCurrentMenu',
+    }),
+    getMenuComponent () {
+      return this.$refs.menuComponent
+    }
+  },
+
+  computed: {
+    ...mapState({
+      currentMenu: state => state.app.currentMenu,
+      mostraConsulta: state => state.app.mostraConsulta
+    })
+  }
+}
+</script>
