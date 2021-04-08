99df7f17d27161b305d2f447dadac676ccbbd8c4
flavio <>
2021-04-08 01:11:32 -0300

    Empresa

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,105 +1,54 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+  <q-layout view="hHh lpR fFf">
 
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
         <q-toolbar-title>
           SIGO - Sistema Integrado de Gestão Operacional
         </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+      <div class="" style="">
+        <q-toolbar class="bg-primary text-white">
+          <q-toolbar-title>Menu</q-toolbar-title>
+        </q-toolbar>
+        <q-list class="bg-grey-6 text-white">
+           <q-item clickable v-ripple>
+              <q-item-section @click="cadastrarEmpresa()">Empresa</q-item-section>
+            </q-item>
+        </q-list>
+      </div>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
+import { mapActions } from 'vuex'
 export default {
-  name: 'MainLayout',
-  components: { EssentialLink },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      left: false
+    }
+  },
+  methods: {
+    ...mapActions({
+      setCurrentMenu: 'app/setCurrentMenu',
+      setCurrentView: 'app/setCurrentView',
+      setMostraConsulta: 'app/setMostraConsulta'
+    }),
+     cadastrarEmpresa () {
+      this.setCurrentMenu('MenuEmpresa')
+      this.setMostraConsulta(true)
+      this.setCurrentView('ConsultaEmpresa')
+      this.left = false
     }
   }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,57 @@
 <template>
   <q-page class="flex flex-center">
-    
+      <principal />
   </q-page>
 </template>
 
 <script>
+import Principal from "src/components/menu/principal.vue"
+import { mapActions, mapState } from 'vuex'
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    Principal
+  },
+
+  computed: {
+    ...mapState({
+      mostraConsulta: state => state.app.mostraConsulta,
+      currentMenu: state => state.app.currentMenu
+    })
+  },
+
+  watch: {
+    splitterModelLocal (value) {
+      this.splitterModelLocal = this.splitterModel
+    }
+  },
+
+  methods: {
+    ...mapActions({
+      setCurrentMenu: 'app/setCurrentMenu',
+      setCurrentView: 'app/setCurrentView',
+      setMostraConsulta: 'app/setMostraConsulta'
+    }),
+    changeCurrentViewConsulta () {
+      this.setMostraConsulta(true)
+      switch (this.currentMenu) {
+        case 'MenuEmpresa':
+          this.setCurrentView('ConsultaEmpresa')
+          break
+      }
+    },
+    changeCurrentViewFormulario () {
+      this.setMostraConsulta(false)
+      switch (this.currentMenu) {
+        case 'MenuEmpresa':
+          this.setCurrentView('FormularioEmpresa')
+          break
+      }
+    }
+  },
+  mounted () {
+    this.setCurrentMenu('')
+  }
 }
 </script>
