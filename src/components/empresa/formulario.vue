c179ca3bce9045605657c6a33846ef5f076d9a59
flavio <>
2021-04-10 00:22:12 -0300

    ajustes no formulario de cadastro

--- src/components/empresa/formulario.vue
+++ src/components/empresa/formulario.vue
@@ -23,6 +23,7 @@
         <div class="row q-gutter-x-md">
           <div class="col" >
             <q-input label="CNPJ" v-model="model.cnpj"
+              mask="##.###.###/####-##"
               outlined class="col-md-6"
               clearable
               filled
@@ -46,8 +47,18 @@
           </div>
         </div>
         <div class="row q-gutter-x-md">
+          <div class="col" >
+            <q-input label="e-mail" v-model="model.email"
+              outlined 
+              clearable
+              filled
+              :rules="[val => !!val || 'Campo obrigatório.', 
+                      val => isValidEmail(val) || 'Formato de e-mail inválido.']" />
+          </div>
+        </div>
+        <div class="row q-gutter-x-md">
             <q-btn label="Cancelar" class="bg-red text-white" @click="cancelar"/>
-            <q-btn label="Salvar" class="bg-green text-white" @click="salvar"/>
+            <q-btn label="Salvar" class="bg-green text-white" @click="acaoSalvar" disable="!isFormValid"/>
         </div>
       </div>
     </div>
@@ -77,6 +88,9 @@ export default {
       setCurrentView: 'app/setCurrentView',
       setMostraConsulta: 'app/setMostraConsulta'
     }),
+    carregarObjeto(obj) {
+      this.model = obj
+    },
     inicializaFormulario () {
       this.model = {}
       this.resetValidationRules();
@@ -91,22 +105,49 @@ export default {
       this.inicializaFormulario()
       this.setView('ConsultaEmpresa')
     }, 
-    salvar () {
-      if(!this.model.cnpj || !this.model.nomeFantasia || !this.model.razaoSocial)  {
+    acaoSalvar () {
+      if(!this.model.cnpj || !this.model.nomeFantasia || !this.model.razaoSocial || !this.model.email)  {
         messagemService.erro('Campos obrigatórios não preechidos')
         return
       }
+
+      if(!this.isValidEmail(this.model.email)) {
+        messagemService.erro('Formato de e-mail inválido')
+        return
+      }
+
+      if(!this.model.id) {
+        this.incluir()
+      } else {
+        this.alterar()
+      }
+    },
+    incluir () {
       this.$q.loading.show()
       empresaService.salvar(this.model).then(response => {
-        debugger
         messagemService.sucesso('Registro salvo com sucesso!')
         this.cancelar()
         this.$q.loading.hide()
       }).catch(erro => {
-        debugger
         messagemService.erro('Ocorreum uma falha ao salvar o registro.')
         this.$q.loading.hide()
       })
+    },
+    alterar () {
+      this.$q.loading.show()
+      empresaService.atualizar(this.model).then(response => {
+        messagemService.sucesso('Registro salvo com sucesso!')
+        this.cancelar()
+        this.$q.loading.hide()
+      }).catch(erro => {
+        messagemService.erro('Ocorreum uma falha ao salvar o registro.')
+        this.$q.loading.hide()
+      })
+    },
+    isValidEmail (obj) {
+      const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
+      console.log(emailPattern.test(obj))
+      return obj !== null && emailPattern.test(obj)
     }
   }
 }
