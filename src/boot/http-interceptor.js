83d16fc5e52228c51178de81b679cdc9677eefb9
flavio <>
2021-04-10 00:21:56 -0300

    Recperar token sessionStorage

--- src/boot/http-interceptor.js
+++ src/boot/http-interceptor.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import authService from 'src/service/authService'
 
 export default ({ store }) => {
   axios.interceptors.request.use(config => {
     const index = config.url.indexOf(process.env.API)
-    if (index !== undefined && index > -1) {
-      config.headers.authorization = ''
+    if (index !== undefined && index > -1 && authService.check()) {
+      config.headers.authorization = authService.token()
     }
     return config
   }, error => {
